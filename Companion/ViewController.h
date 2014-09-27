c91b8c3d40c9aabc665a5da7d6902282bbbf4978
Eduardo Carrillo <eduardo.calbor@gmail.com>
2014-09-27 13:56:25 -0500

    Initial Commit

---
+++ Companion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Companion
+//
+//  Created by Eduardo Antoni Carrillo Albor on 9/27/14.
+//  Copyright (c) 2014 Keduardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Companion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Companion
+//
+//  Created by Eduardo Antoni Carrillo Albor on 9/27/14.
+//  Copyright (c) 2014 Keduardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
